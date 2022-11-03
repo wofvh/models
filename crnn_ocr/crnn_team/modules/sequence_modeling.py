a0909130e3ca426a060f1e7920596e04e5a7ee1a
wofvh <wofvh@naver.com>
2022-11-03 18:01:29 +0900

    test

---
+++ crnn_ocr/crnn_team/modules/sequence_modeling.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class BidirectionalLSTM(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(BidirectionalLSTM, self).__init__()
+        self.rnn = nn.LSTM(input_size, hidden_size, bidirectional=True, batch_first=True) # nn.LSTM : output shape = (batch_size, seq_len, hidden_size * num_directions)
+        self.linear = nn.Linear(hidden_size * 2, output_size) # nn
+
+    def forward(self, input):
+        """
+        input : visual feature [batch_size x T x input_size]
+        output : contextual feature [batch_size x T x output_size]
+        """
+        self.rnn.flatten_parameters() # flatten_parameters는 RNN의 파라미터를 평평하게 만들어주는 역할을 한다
+        recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
+        output = self.linear(recurrent)  # batch_size x T x output_size
+        return output
